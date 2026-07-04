906cb39feb0dad3887be00d9b82f55744d30d7e4
ItsNafisa <nafisababiker535@gmail.com>
2026-07-04 12:39:17 +0200

    changing in js

--- script.js
+++ script.js
@@ -19,3 +19,63 @@ links.forEach(function(link) {
     }
 });
 
+
+
+const mySection = document.querySelector('.section3');
+const allSpans = document.querySelectorAll('.section3 .content .box div span');
+const skillsObserver = new IntersectionObserver(function(entries, self) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            allSpans.forEach(function(span) {
+                span.style.width = span.getAttribute('data-width');
+            });
+            self.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.15 
+});
+if (mySection) {
+    skillsObserver.observe(mySection);
+}
+
+
+
+
+
+const statsSection = document.querySelector('.section4');
+const statNumbers = document.querySelectorAll('.section4 .box h3');
+let started = false; 
+
+function startCount(element) {
+    const goal = parseInt(element.getAttribute('data-goal')); 
+    
+    const counter = setInterval(() => {
+        
+        let currentText = parseInt(element.textContent);
+        element.textContent = currentText + 1;
+        
+        
+        if (parseInt(element.textContent) === goal) {
+            clearInterval(counter);
+        }
+    }, 2000 / goal); 
+}
+
+
+const statsObserver = new IntersectionObserver((entries, self) => {
+    entries.forEach(entry => {
+        
+        if (entry.isIntersecting && !started) {
+            statNumbers.forEach(num => startCount(num));
+            started = true; 
+            self.unobserve(entry.target); 
+        }
+    });
+}, {
+    threshold: 0.2 
+});
+if (statsSection) {
+    statsObserver.observe(statsSection);
+}
+
